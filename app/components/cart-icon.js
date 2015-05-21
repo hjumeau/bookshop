d1ce6a2fb4b5747526cb9b4095c98b81a08e368e
Harry JUMEAU <harry.jumea@gmail.com>
2015-05-22 01:18:27 +0200

    Fix responsive bug and change some wording

--- app/components/cart-icon.js
+++ app/components/cart-icon.js
@@ -2,6 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-	classNames:['cart-icon']
+	classNames:['cart-icon'],
+
+	nbrArticleText: function(){
+		return this.get('cart.nbrArticles') > 1 ? 'articles' : 'article'; 
+	}.property('cart.nbrArticles')
 
 });
